95275444ccddada5f7c559ee496cbbe4a907d473
unknown <you@example.com>
2023-06-26 07:50:38 +0900

    report_donate

---
+++ src/main/java/com/teamdonate/ReportDonateApplication.java
@@ -0,0 +1,13 @@
+package com.teamdonate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportDonateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportDonateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamdonate/ReportDonateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamdonate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportDonateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
